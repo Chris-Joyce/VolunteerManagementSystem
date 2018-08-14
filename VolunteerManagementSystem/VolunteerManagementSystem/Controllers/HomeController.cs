99600840714ea28b89546d5ad20ff28c1f9b99aa
Chris-Joyce <36461109+Chris-Joyce@users.noreply.github.com>
2018-08-14 14:11:30 -0400

    Add files via upload

---
+++ VolunteerManagementSystem/VolunteerManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VolunteerManagementSystem.Models;
+
+namespace VolunteerManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(Login model)
+        {
+           
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+        public IActionResult ManageVolunteers()
+        {
+            return View();
+        }
+        public IActionResult ManageOpportunities()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VolunteerManagementSystem/VolunteerManagementSystem/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolunteerManagementSystem.Models
+{
+    public class Login
+    {
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+
+    }
+}
