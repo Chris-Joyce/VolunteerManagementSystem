74d9271debfcdfc79b62939e594e38e7807320cb
KarlaClifford <41645492+KarlaClifford@users.noreply.github.com>
2018-08-21 20:54:18 -0400

    Add files via upload

---
+++ PreSemesterProject/PreSemesterProject/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PreSemesterProject.Models;
+
+namespace PreSemesterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult AddNewVolunteer()
+        {
+            return View();
+        }
+        public IActionResult VolunteerSearch()
+        {
+            return View();
+        }
+        public IActionResult OpportunityMatches()
+        {
+            return View();
+        }
+        public IActionResult VolunteerProfile()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
