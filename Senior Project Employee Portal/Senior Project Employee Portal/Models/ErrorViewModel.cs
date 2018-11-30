8a9a95dc6c0573818737339ebb590db5dbd9866e
danleee <42387485+danleee@users.noreply.github.com>
2018-11-29 20:24:47 -0500

    Add files via upload

---
+++ Senior Project Employee Portal/Senior Project Employee Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Senior_Project_Employee_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
